1ac44456215ff5e89986d917f70aeeb52f81cef0
Sami Jakonen <sami.jakonen@iki.fi>
2022-12-05 11:23:42 +0200

    Add the first test and configure nyc

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -1,1 +1,17 @@
-// placeholder 1
+/*
+COMP.SE.200-2022 Software testing
+Unit tests for ceil.js
+Lotta Lindell
+Sami Jakonen
+*/
+import ceil from '../src/ceil.js';
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('#ceil()', function() {
+    context('given a single float argument', function() {
+        it('should return the given value rounded up to the nearest integer', function() {
+            expect(ceil(6.087)).to.equal(7);
+        });
+    });
+});
